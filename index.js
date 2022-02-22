54968ef9c0b3fcd5ca2c47e9a4d5b3b34b69afb6
Kmamrutkar91 <60057262+Kmamrutkar91@users.noreply.github.com>
2022-02-22 09:48:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+// import express from 'express'
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+const customers = [
+    {firstName: 'John', age: 27},
+    {firstName: 'James', age: 32},
+    {firstName: 'Robert', age: 45},
+    {firstName: 'Kalyani', age: 21},
+]
+
+app.get('/',(req, resp) =>{
+    resp.send("Welcome to Customers API")
+})
+
+app.get('/customers', (req,resp) =>{
+    resp.send(customers)
+})
+
+app.get('/customers/:age', (req,resp) =>{
+    const customer = customers.find(k => k.age === parseInt(req.params.age) )
+    if (!customer) resp.status(404).send("Customer not found")
+    resp.send(customer)
+})
+
+app.get('/findFirstName/:firstName', (req,resp) =>{
+    console.log(req.params.firstName)
+    const customername = customers.find(k => k.firstName === (req.params.firstName))
+    if (!customername) resp.status(404).send("Customer not found")
+    resp.send(customername) 
+})
+
+app.post('/customers/addCustomer',(req, resp) =>{
+    const newCustomer ={
+        firstName: req.body.firstName,
+        age: req.body.age
+    }
+    customers.push(newCustomer)
+    resp.send(newCustomer)
+})
+
+app.put('/customers/:age', (req,resp) =>{
+    const customer = customers.find(k => k.age === parseInt(req.params.age))
+    if (!customer) resp.status(404).send("Customer not found")
+    customer.firstName = req.body.firstName
+    resp.send(customer)
+})
+
+app.delete('/customers/:age', (req,resp)=>{
+    const customer = customers.find(k => k.age === parseInt(req.params.age))
+    if (!customer) resp.status(404).send("Customer not found")
+    const index = customers.indexOf(customer)
+    customers.splice(index,1)
+    resp.send(customer)
+})
+
+app.listen(3000)
---
+++ mongodb.js
@@ -0,0 +1,70 @@
+const express = require('express')
+const MongoClient = require('mongodb').MongoClient
+const app = express()
+app.use(express.json())
+var database
+
+app.get('/', (req, resp) => {
+    resp.send("Welcome to mongodb API")
+})
+
+app.get('/products', (req, resp) => {
+    database.collection('products').find({}).toArray((err, result) => {
+        if (err) throw error
+        resp.send(result)
+    })
+})
+
+app.get('/products/:id',(req, resp) => {
+    database.collection('products').find({id: parseInt(req.params.id)}).toArray((err, result) => {
+        if(err) throw err
+        resp.send(result)
+    })
+})
+
+app.post('/products/addProduct', (req, resp)=>{
+    let res = database.collection('products').find({}).sort({id: -1}).limit(1)
+    res.forEach(obj =>{
+        if(obj){
+            let product ={
+                id: obj.id+1,
+                name: req.body.product,
+                price: req.body.price
+               
+            }
+            database.collection('products').insertOne(product, (err, result) =>{
+                if(err) resp.status(500).send(err)
+                resp.send("Added Successfully")
+            })
+        }
+    })
+})
+app.put('/products/:id',(req, resp) => {
+    let query = {id: parseInt(req.params.id)}
+   let product = {
+    id: parseInt(req.params.id),
+    name: req.body.name
+    }
+    let dataset = {$set: product}
+    database.collection('products').updateOne(
+        query, 
+       dataset , (err, result) =>{
+        if(err) throw err
+        resp.send(product)
+    })
+})
+ 
+app.delete('/products/:id', (req, resp) => {
+    database.collection('products').deleteOne({id: parseInt(req.params.id)}, (err, result) =>{
+        if(err) throw err
+        resp.send('Book is deleted')
+    })
+})
+
+app.listen(8080, () => {
+    MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true }, (error, result) => {
+        if (error) throw error
+        database = result.db('mydatabase')
+        console.log("CONEECTION SUCCESSFUL!!")
+    })
+})
